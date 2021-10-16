345a181a819db56504f3e947ca38368971d8e243
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2021-10-16 12:38:17 -0300

    Projeto criado

---
+++ src/main/java/com/volaxxy/cpjavamongodb/CpjavamongodbApplication.java
@@ -0,0 +1,13 @@
+package com.volaxxy.cpjavamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpjavamongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpjavamongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volaxxy/cpjavamongodb/CpjavamongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volaxxy.cpjavamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpjavamongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
