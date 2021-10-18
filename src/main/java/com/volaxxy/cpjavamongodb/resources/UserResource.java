55e3996273b22d3b391f738d118bed11a8276af5
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2021-10-17 22:45:21 -0300

    Entity User and REST working

---
+++ src/main/java/com/volaxxy/cpjavamongodb/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.volaxxy.cpjavamongodb.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.volaxxy.cpjavamongodb.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll() {
+		User u1 = new User("1", "Maria Silva", "maria@gmail.com");
+		User u2 = new User("2", "Alex Green", "alex@gmail.com");
+		
+		List<User> users = new ArrayList<>();
+		users.addAll(Arrays.asList(u1, u2));
+		
+		return ResponseEntity.ok(users);
+	}
+
+}
