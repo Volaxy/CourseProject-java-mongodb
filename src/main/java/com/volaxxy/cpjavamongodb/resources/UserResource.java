0652250a0b986f2ddf40cd909ebc52c9c2db3861
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2021-10-22 21:03:01 -0300

    Using DTO Pattern to Return Users

--- src/main/java/com/volaxxy/cpjavamongodb/resources/UserResource.java
+++ src/main/java/com/volaxxy/cpjavamongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.volaxxy.cpjavamongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.volaxxy.cpjavamongodb.domain.User;
+import com.volaxxy.cpjavamongodb.dto.UserDTO;
 import com.volaxxy.cpjavamongodb.services.UserService;
 
 @RestController
@@ -19,10 +21,12 @@ public class UserResource {
 	private UserService userService;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> users = userService.findAll();
 		
-		return ResponseEntity.ok(users);
+		List<UserDTO> usersDTO = users.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok(usersDTO);
 	}
 
 }
