df681b819faa7c80f1766992a84453279fe131df
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2021-10-23 12:24:15 -0300

    Getting a Post by Id

---
+++ src/main/java/com/volaxxy/cpjavamongodb/services/PostService.java
@@ -0,0 +1,24 @@
+package com.volaxxy.cpjavamongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.volaxxy.cpjavamongodb.domain.Post;
+import com.volaxxy.cpjavamongodb.repositories.PostRepository;
+import com.volaxxy.cpjavamongodb.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = postRepository.findById(id);
+		
+		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found! :("));
+	}
+
+}
