8058678554b36ba9e14e0700aa26c2849c770a14
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2021-10-22 23:00:35 -0300

    User Deleting with DELETE Method

--- src/main/java/com/volaxxy/cpjavamongodb/services/UserService.java
+++ src/main/java/com/volaxxy/cpjavamongodb/services/UserService.java
@@ -34,5 +34,10 @@ public class UserService {
 	public User fromDTO(UserDTO userDTO) {
 		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
 
 }
