82d6029a94157cc123ea7d25b2783345e615e3d3
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2021-10-23 13:34:36 -0300

    Query with Multiple Criteria

--- src/main/java/com/volaxxy/cpjavamongodb/services/PostService.java
+++ src/main/java/com/volaxxy/cpjavamongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.volaxxy.cpjavamongodb.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,12 @@ public class PostService {
 	
 	public List<Post> findByTittle(String text) {
 		return postRepository.findByTittle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		
+		return postRepository.fullSearch(text, minDate, maxDate);
 	}
 
 }
